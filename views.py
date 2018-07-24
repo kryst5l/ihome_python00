9242ed70fdbc676e926f2cf495b90082ad29a44f
liujian <506660782@qq.com>
2018-07-24 10:24:22 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 
 from django.http import Httpresponse
+from django.shortcuts import redirect
 
 
 def index(request):
     return HttpResponse("index")
+
+def login(request):
+    return redirect("/index")
